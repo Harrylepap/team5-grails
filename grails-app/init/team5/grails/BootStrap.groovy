9d2c23a933a12fcbcb793bb4ea3a34b77a2b9bbb
Riana9711 <riana.back@gmail.com>
2022-03-21 22:19:00 +0300

    feat: Bases de spring security (1)

--- grails-app/init/team5/grails/BootStrap.groovy
+++ grails-app/init/team5/grails/BootStrap.groovy
@@ -1,102 +1,45 @@
 package team5.grails
 
-import javax.transaction.Transactional
-
 class BootStrap {
 
     def init = { servletContext ->
-        anotherMagicalData()
+        test()
     }
     def destroy = {
     }
 
-    /**
-     * Generation d'utilisateur et de produit
-     */
-    @Transactional
-    def addUtilisateurAndProduit() {
-        ["Harrylepap", "Sitraka", "Riana"].each { String username ->
-            def utilisateurInstance = new Utilisateur(username: username, email: "$username@ituniversity-mg.com", createdAt: new Date())
-            (1..5).each { Integer index ->
-                def produitInstance = new Produit(
-                        libelle: "Titre $username - $index",
-                        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. $utilisateurInstance@ituniversity-mg.com",
-                        prix: 2 * index,
-                        active: Boolean.TRUE
-                )
-                if (!produitInstance.save())
-                    System.out.println("Le produit ne peut pas être sauvegarder")
-            }
-            if (!utilisateurInstance.save())
-                System.out.println("L'utilisateur ne peut pas être sauvegarder")
-        }
-    }
+    void test(){
 
-    @Transactional
-    def testAddProduct() {
+        def adminRole = new Role(authority: "ROLE_ADMIN").save();
+        def clientRole = new Role(authority:  "ROLE_CLIENT").save();
 
-        for (int i = 1; i<= 10; i++){
-            def utilisateurInstance = new Utilisateur(username: "harrylepap", email: "rbako@ituniversity-mg.com", createdAt: new Date())
-            utilisateurInstance.save()
-            def produitInstance = new Produit(
-                    libelle: "Test titre",
-                    description: "Description Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                    prix: 12,
-                    dateCreated: new Date(),
-                    lastUpdated: new Date(),
-                    auteur: utilisateurInstance,
-                    active: Boolean.TRUE,
-                    images: [new Image(filename: "produits-.jpg")]
-            )
-            if (!produitInstance.save(flush: true)) {
-                produitInstance.errors.allErrors.each {
-                    println it
-                }
-            }
-        }
+        def admin = new Utilisateur(username:  "admin", password: "admin").save();
+        UtilisateurRole.create(admin, adminRole);
 
-    }
-
-    @Transactional
-    def anotherMagicalData(){
-        ["harry", "sandie"].each {String username ->
-            def utilisateurInstance = new Utilisateur(username: username, email: "$username@ituniversity-mg.com", createdAt: new Date())
-            (1..5).each {
-                Integer index ->
-                    def produitInstance = new Produit(
-                            libelle: "Test titre",
-                            description: "Description Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                            prix: 12,
-                            dateCreated: new Date(),
-                            lastUpdated: new Date(),
-                            auteur: utilisateurInstance,
-                            active: Boolean.TRUE,
-                    )
-                    (1..3).each {
-                        produitInstance.addToImages( new Image(filename: "fichier_$username-$index-$it-a.jpg"))
-                        produitInstance.errors.getAllErrors().each {
-                            println(it)
+        ["riana", "tanjona"].each {
+            String username ->
+                def userInstance = new Utilisateur(username: username, password: "password")
+                (1..5).each {
+                    Integer index ->
+                        def annonceInstance = new Annonce(
+                                title: "Title $username $index",
+                                description: "Description de l'annonce $username $index",
+                                price: 100*index,
+                                active: Boolean.TRUE)
+                        (1..5).each {
+                            annonceInstance.addToIllustrations(
+                                    new Illustration(filename: "filename_$username-$index-$it")
+                            )
                         }
-                    }
-                    utilisateurInstance.addToProduits(produitInstance)
-                    utilisateurInstance.save()
-            }
-        }
+                        userInstance.addToAnnonces(annonceInstance)
+                        userInstance.save(flush: true, failOnError: true)
+                }
+                UtilisateurRole.create(userInstance, clientRole)
 
-        insertAnnonce()
-    }
-
-    @Transactional
-    def insertAnnonce(){
-        (1..3).each {
-            def annonceInstance = new Annonce(
-                    libelle: "Test $it",
-                    description: "Hola",
-                    active: Boolean.TRUE,
-                    dateCreated: new Date(),
-                    image: new Image(filename: "$it-0.jpeg")
-            )
-            annonceInstance.save()
+                UtilisateurRole.withSession {
+                    it.flush()
+                    it.clear()
+                }
         }
     }
 }
