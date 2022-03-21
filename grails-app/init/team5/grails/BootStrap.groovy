9236fc43f9383bfc9c4afe754e3437b28921d78e
Riana9711 <riana.back@gmail.com>
2022-03-21 23:26:05 +0300

    test : donnée statique

--- grails-app/init/team5/grails/BootStrap.groovy
+++ grails-app/init/team5/grails/BootStrap.groovy
@@ -1,5 +1,7 @@
 package team5.grails
 
+import javax.transaction.Transactional
+
 class BootStrap {
 
     def init = { servletContext ->
@@ -8,6 +10,7 @@ class BootStrap {
     def destroy = {
     }
 
+    @Transactional
     void test(){
 
         def adminRole = new Role(authority: "ROLE_ADMIN").save();
@@ -16,23 +19,28 @@ class BootStrap {
         def admin = new Utilisateur(username:  "admin", password: "admin").save();
         UtilisateurRole.create(admin, adminRole);
 
-        ["riana", "tanjona"].each {
+        ["riana", "sitraka", "bako"].each {
             String username ->
                 def userInstance = new Utilisateur(username: username, password: "password")
                 (1..5).each {
                     Integer index ->
-                        def annonceInstance = new Annonce(
-                                title: "Title $username $index",
-                                description: "Description de l'annonce $username $index",
-                                price: 100*index,
-                                active: Boolean.TRUE)
-                        (1..5).each {
-                            annonceInstance.addToIllustrations(
-                                    new Illustration(filename: "filename_$username-$index-$it")
-                            )
+                        def produitInstance = new Produit(
+                                libelle: "Test titre",
+                                description: "Description Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                prix: 12,
+                                dateCreated: new Date(),
+                                lastUpdated: new Date(),
+                                auteur: userInstance,
+                                active: Boolean.TRUE,
+                        )
+                        (1..3).each {
+                            produitInstance.addToImages( new Image(filename: "fichier_$username-$index-$it-a.jpg"))
+                            produitInstance.errors.getAllErrors().each {
+                                println(it)
+                            }
                         }
-                        userInstance.addToAnnonces(annonceInstance)
-                        userInstance.save(flush: true, failOnError: true)
+                        userInstance.addToProduits(produitInstance)
+                        userInstance.save()
                 }
                 UtilisateurRole.create(userInstance, clientRole)
 
